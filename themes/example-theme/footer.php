12f3532dd91a0678ed466e8941bdda14e01d0d2e
ApplianceJohn <thednbgremlin@gmail.com>
2022-05-19 14:54:26 -0400

    added basic template and navigation menu

---
+++ themes/example-theme/footer.php
@@ -0,0 +1,4 @@
+<footer><span>Footer content</span></footer>
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ themes/example-theme/index.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+<main id="content">
+	<h1 class="text-6xl leading-none font-extrabold">This is a content test.</h1>
+	<p>Some content!</p>
+</main>
+<?php
+get_footer();
+?>
